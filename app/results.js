a09f969a19e027b0d8b1dc4298eb6c37a8585cb5
matt weems <mweems69@yahoo.com>
2015-07-08 11:26:46 +0100

    commit

---
+++ app/results.js
@@ -0,0 +1,19 @@
+(function(){
+    var app = angular.module('sessionResults', []);
+    app.controller('resultsController', function(){
+        this.playerName = name
+        this.wonLost = money
+        this.totalHands = hands
+        this.handsWon = handsWon
+        this.percentHandsWone = percentHandsWon
+        this.wwsf = wwsf
+        this.allInEquity = allInEquity
+    });
+    var name = "mweems1"
+    var money = 3
+    var hands = 250
+    var handsWon = 25
+    var percentHandsWon = 10
+    var wwsf = 42
+    var allInEquity = 44
+})();
